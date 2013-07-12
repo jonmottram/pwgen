2c38acc16f7df8959d6b9edc9ce2d57552f80003
Jon Mottram <jmottram@jomos-laptop.rtlogic.com>
2013-07-12 10:26:34 -0600

    Add the script, and update the README.

--- README.md
+++ README.md
@@ -2,3 +2,16 @@ pwgen
 =====
 
 Simple bash script to generate tabula recta with some sort-of random characters
+
+Usage
+-----
+Run the program :
+    ./pwgen.sh
+
+Print out the generated table, and use it as characters for your passwords.
+
+See Also
+--------
+<http://en.wikipedia.org/wiki/Tabula_recta>
+
+<http://blog.jgc.org/2010/12/write-your-passwords-down.html>
---
+++ pwgen.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+###
+##  For generating a tabula recta with some sort-of random characters
+###
+
+echo "   A B C D E F G H I J K L M N O P Q R S T U V W X Y Z " ; echo " +----------------------------------------------------" ; for i in A B C D E F G H I J K L M N O P Q R S T U V W X Y Z ; do echo -n "$i|" ; openssl rand -base64 20 | cut -c-26 | sed -e's/\(.\)/ \1/g' ; done
