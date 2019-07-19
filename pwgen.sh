8769ba5cd39e7c9b8724cd361a51af9095a9d3a5
Jon Mottram <jonmottram@gmail.com>
2019-07-18 21:23:00 -0600

    Split the one line into several (easier to read), and increase the number of characters being output.

--- pwgen.sh
+++ pwgen.sh
@@ -4,4 +4,10 @@
 ##  For generating a tabula recta with some sort-of random characters
 ###
 
-echo "   A B C D E F G H I J K L M N O P Q R S T U V W X Y Z " ; echo " +----------------------------------------------------" ; for i in A B C D E F G H I J K L M N O P Q R S T U V W X Y Z ; do echo -n "$i|" ; openssl rand -base64 20 | cut -c-26 | sed -e's/\(.\)/ \1/g' ; done
+echo "   A B C D E F G H I J K L M N O P Q R S T U V W X Y Z "
+echo " +----------------------------------------------------"
+for i in A B C D E F G H I J K L M N O P Q R S T U V W X Y Z
+do
+    echo -n "$i|"
+    openssl rand -base64 30 | cut -c-26 | sed -e's/\(.\)/ \1/g'
+done
